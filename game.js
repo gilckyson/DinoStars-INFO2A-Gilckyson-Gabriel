16f62d19fc201717bfd1123a4ea311f7b27cf5c5
gilckyson <gilckysongabriel@gmail.com>
2025-08-09 21:10:38 -0300

    Update game.js

--- game.js
+++ game.js
@@ -9,37 +9,43 @@ const mainMenu = document.getElementById('mainMenu');
 const gameScreen = document.getElementById('gameScreen');
 
 const dinoImage = new Image();
-dinoImage.src = 'scott.gif';
+dinoImage.src = 'dinossauro.png';
 
 const obstacleSmallImage = new Image();
-obstacleSmallImage.src = 'obstaculo.png';
+obstacleSmallImage.src = 'cacto.png';
 
 const obstacleLargeImage = new Image();
-obstacleLargeImage.src = 'cano.png';
+obstacleLargeImage.src = 'cactoG.png';
+
+const backgroundImage = new Image();
+backgroundImage.src = 'ceu.jpg'; 
 
 const GAME = {
     GRAVITY: 0.5,
-    JUMP_POWER: -12,
-    OBSTACLE_SPEED: 5,
-    SHOOT_COOLDOWN_TIME: 180,
+    JUMP_POWER: -10,
+    OBSTACLE_SPEED: 9,
+    SHOOT_COOLDOWN_TIME: 130,
+    OBSTACLE_COOLDOWN_TIME: 25,
     GROUND_HEIGHT: 50,
 };
 
-let dino, obstacles, bullets, score, shootCooldown, isGameOver;
+let dino, obstacles, bullets, score, shootCooldown, isGameOver, obstacleCooldown;
 
 function createDino() {
     return {
-        x: 50, y: 200, width: 40, height: 60,
+        x: 50, y: 200, width: 50, height: 50,
         velocityY: 0, isOnGround: false
     };
 }
 
 function createObstacle() {
     const isSmall = Math.random() > 0.5;
-    const width = isSmall ? 30 : 60;
+    const width = isSmall ? 50 : 70;
     const height = isSmall ? 50 : 70;
     return {
-        width: width, height: height, x: canvas.width,
+        width: width, 
+        height: height, 
+        x: canvas.width,
         y: canvas.height - GAME.GROUND_HEIGHT - height
     };
 }
@@ -57,6 +63,7 @@ function resetGame() {
     bullets = [];
     score = 0;
     shootCooldown = 0;
+    obstacleCooldown = 0;
     isGameOver = false;
     
     scoreDisplay.innerText = `Pontos: 0`;
@@ -105,9 +112,14 @@ function updateGame() {
         dino.velocityY = 0;
         dino.isOnGround = true;
     }
-
-    if (Math.random() < 0.015) {
-        obstacles.push(createObstacle());
+    
+    if (obstacleCooldown > 0) {
+        obstacleCooldown--;
+    } else {
+        if (Math.random() < 0.015) {
+            obstacles.push(createObstacle());
+            obstacleCooldown = GAME.OBSTACLE_COOLDOWN_TIME;
+        }
     }
     obstacles.forEach(o => o.x -= GAME.OBSTACLE_SPEED);
     obstacles = obstacles.filter(o => o.x + o.width > 0);
@@ -123,7 +135,7 @@ function updateGame() {
         cooldownDisplay.innerText = "Tiro Pronto!";
         cooldownDisplay.style.color = 'green';
     }
-
+    
     handleCollisions();
     
     score++;
@@ -131,14 +143,15 @@ function updateGame() {
 }
 
 function drawGame() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
     
-    ctx.fillStyle = '#535353';
+    ctx.fillStyle = '#000000';
     ctx.fillRect(0, canvas.height - GAME.GROUND_HEIGHT, canvas.width, GAME.GROUND_HEIGHT);
+    
     ctx.drawImage(dinoImage, dino.x, dino.y, dino.width, dino.height);
     
     obstacles.forEach(o => {
-        if (o.width <= 35) {
+        if (o.width < 60) { 
             ctx.drawImage(obstacleSmallImage, o.x, o.y, o.width, o.height);
         } else {
             ctx.drawImage(obstacleLargeImage, o.x, o.y, o.width, o.height);
@@ -159,6 +172,7 @@ function gameLoop() {
     drawGame();
     if (!isGameOver) requestAnimationFrame(gameLoop);
 }
+
 startButton.addEventListener('click', () => {
     mainMenu.classList.add('hidden');
     gameScreen.classList.remove('hidden');
